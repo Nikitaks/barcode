2e969d76f033973bbfe8f1d995f8b15ebaeabd81
Nikitaks <n.aksenov2015@yandex.ru>
2023-09-26 10:54:18 +0300

    fetching proteins, fat and carbohydrates

--- MessageProcessing/src/main/java/com/barcode/messageprocesor/dto/Food.java
+++ MessageProcessing/src/main/java/com/barcode/messageprocesor/dto/Food.java
@@ -18,10 +18,22 @@ public class Food {
 	private int energy100g;
 	private String energyUnit;
 	private int energyValue;
-	
+	private int proteins;
+	private int proteins_100g;
+	private int fat;
+	private int fat_100g;
+	private int carbohydrates;
+	private int carbohydrates_100g;
+
 	@JsonProperty("product")
 	private void nutrimentEnergy(JsonNode product) { 
 		JsonNode nutriments = product.get("nutriments");
+		proteins = nutriments.get("proteins").asInt();
+		proteins_100g = nutriments.get("proteins_100g").asInt();
+		fat = nutriments.get("fat").asInt();
+		fat_100g = nutriments.get("fat_100g").asInt();
+		carbohydrates = nutriments.get("carbohydrates").asInt();
+		carbohydrates_100g = nutriments.get("carbohydrates_100g").asInt();
 		nutrimentsEnergy = nutriments.get("energy").asInt(); 
 		energyKj = nutriments.get("energy-kj").asInt(); 
 		energyKj100g = nutriments.get("energy-kj_100g").asInt();
@@ -31,7 +43,6 @@ public class Food {
 		energy100g = nutriments.get("energy_100g").asInt();
 		energyUnit = nutriments.get("energy_unit").asText();
 		energyValue = nutriments.get("energy_value").asInt();
-		//return nutrimentsEnergy;
 	}
 
 	public String getCode() {
@@ -130,13 +141,63 @@ public class Food {
 		this.energyValue = energyValue;
 	}
 
+	public int getProteins() {
+		return proteins;
+	}
+
+	public void setProteins(int proteins) {
+		this.proteins = proteins;
+	}
+
+	public int getProteins_100g() {
+		return proteins_100g;
+	}
+
+	public void setProteins_100g(int proteins_100g) {
+		this.proteins_100g = proteins_100g;
+	}
+
+	public int getFat() {
+		return fat;
+	}
+
+	public void setFat(int fat) {
+		this.fat = fat;
+	}
+
+	public int getFat_100g() {
+		return fat_100g;
+	}
+
+	public void setFat_100g(int fat_100g) {
+		this.fat_100g = fat_100g;
+	}
+
+	public int getCarbohydrates() {
+		return carbohydrates;
+	}
+
+	public void setCarbohydrates(int carbohydrates) {
+		this.carbohydrates = carbohydrates;
+	}
+
+	public int getCarbohydrates_100g() {
+		return carbohydrates_100g;
+	}
+
+	public void setCarbohydrates_100g(int carbohydrates_100g) {
+		this.carbohydrates_100g = carbohydrates_100g;
+	}
+
 	@Override
 	public String toString() {
 		return "Food [code=" + code + ", status=" + status + ", status_verbose=" + status_verbose
 				+ ", nutrimentsEnergy=" + nutrimentsEnergy + ", energyKj=" + energyKj + ", energyKj100g=" + energyKj100g
 				+ ", energyKjUnit=" + energyKjUnit + ", energyKjValue=" + energyKjValue + ", energyKjValueComputed="
 				+ energyKjValueComputed + ", energy100g=" + energy100g + ", energyUnit=" + energyUnit + ", energyValue="
-				+ energyValue + "]";
+				+ energyValue + ", proteins=" + proteins + ", proteins_100g=" + proteins_100g + ", fat=" + fat
+				+ ", fat_100g=" + fat_100g + ", carbohydrates=" + carbohydrates + ", carbohydrates_100g="
+				+ carbohydrates_100g + "]";
 	}
 
 }
